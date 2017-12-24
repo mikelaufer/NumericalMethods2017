14aba6c4e20a324fea0863be853e174269858e7e
mike <mjlaufer103@gmail.com>
2017-12-25 00:07:42 +0200

    all done with hw6

---
+++ hw6/auto/hw6.el
@@ -0,0 +1,51 @@
+(TeX-add-style-hook
+ "hw6"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "{preamble"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted")
+   (LaTeX-add-labels
+    "sec:org40d2a12"
+    "sec:orgd0e4425"
+    "sec:org2ea51fa"
+    "sec:org4d29ba2"
+    "sec:org75214ca"
+    "sec:org8ff358f"
+    "sec:orgc3b83dd"
+    "sec:org3ff70c5"
+    "sec:org03d2b5e"
+    "sec:org47608ef"
+    "sec:orgcbebb65"
+    "sec:orgca138ea"
+    "sec:org3c31771"
+    "sec:org63f96d8"))
+ :latex)
+
