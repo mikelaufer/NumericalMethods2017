772d2f2eeaa6153e1db42b077cf07ebbd09f776d
mike <mjlaufer103@gmail.com>
2018-01-18 07:05:33 +0200

    added heatpipe

--- hw6/auto/hw6.el
+++ hw6/auto/hw6.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,19 +33,19 @@
     "hyperref"
     "minted")
    (LaTeX-add-labels
-    "sec:org40d2a12"
-    "sec:orgd0e4425"
-    "sec:org2ea51fa"
-    "sec:org4d29ba2"
-    "sec:org75214ca"
-    "sec:org8ff358f"
-    "sec:orgc3b83dd"
-    "sec:org3ff70c5"
-    "sec:org03d2b5e"
-    "sec:org47608ef"
-    "sec:orgcbebb65"
-    "sec:orgca138ea"
-    "sec:org3c31771"
-    "sec:org63f96d8"))
+    "sec:org1f6dba0"
+    "sec:org2f19939"
+    "sec:org4643e82"
+    "sec:org7abc4f4"
+    "sec:org7840564"
+    "sec:org23f080c"
+    "sec:orga91b7ca"
+    "sec:orgc49b7d5"
+    "sec:org4d718f5"
+    "sec:org435ff03"
+    "sec:org919cbac"
+    "sec:org67d9f2b"
+    "sec:orgdac6ee5"
+    "sec:org1193ae5"))
  :latex)
 
